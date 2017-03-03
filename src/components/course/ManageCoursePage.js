872aca49eb4fe4ac5d414373a39b5d8ce6ece3c9
David Gonzales <David Gonzales>
2017-03-03 09:35:51 -0700

    cleaning up local machine

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -1,65 +1,67 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
 import toastr from 'toastr';
 
-class ManageCoursePage extends React.Component {
-    constructor(props, context) {
-      super(props, context);
-      this.state = {
-        course: Object.assign({}, this.props.course),
-        errors: {}
-      };
+class ManageCoursePage extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
 
-      this.updateCourseState = this.updateCourseState.bind(this);
-      this.saveCourse = this.saveCourse.bind(this);
-    }
+    this.updateCourseState = this.updateCourseState.bind(this);
+    this.saveCourse = this.saveCourse.bind(this);
+  }
 
-    componentWillReceiveProps(nextProps){
-      if(this.props.course.id != nextProps.course.id){
-        this.setState({course: Object.assign({}, nextProps.course)});
-      }
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id != nextProps.course.id) {
+      this.setState({course: Object.assign({}, nextProps.course)});
     }
+  }
 
-    updateCourseState(event){
-      const field = event.target.name;
-      let course = this.state.course;
-      course[field] = event.target.value;
-      return this.setState({course:course});
-    }
-    redirect() {
-      this.setState({saving: false});
-      toastr.success('Course Saved');
-      this.context.router.push('/courses');
-    }
-    saveCourse(event){
-      event.preventDefault();
-      this.setState({saving: true});
-      this.props.actions.saveCourse(this.state.course)
-        .then(() => this.redirect())
-        .catch(error => {
-          toastr.error(error);
-          this.setState({saving: false});
-        });
-    }
+  updateCourseState(event) {
+    const field = event.target.name;
+    let course = this.state.course;
+    course[field] = event.target.value;
+    return this.setState({course: course});
+  }
 
-    render() {
-        return (
-          <div>
-            <h1>Manage Course</h1>
-            <CourseForm
-              allAuthors={this.props.authors}
-              onChange={this.updateCourseState}
-              onSave={this.saveCourse}
-              course={this.state.course}
-              errors={this.state.errors}
-              saving={this.state.saving}
-            />
-          </div>
-        );
-    }
+  redirect() {
+    this.setState({saving: false});
+    toastr.success('Course Saved');
+    this.context.router.push('/courses');
+  }
+
+  saveCourse(event) {
+    event.preventDefault();
+    this.setState({saving: true});
+    this.props.actions.saveCourse(this.state.course)
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error);
+        this.setState({saving: false});
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Manage Course</h1>
+        <CourseForm
+          allAuthors={this.props.authors}
+          onChange={this.updateCourseState}
+          onSave={this.saveCourse}
+          course={this.state.course}
+          errors={this.state.errors}
+          saving={this.state.saving}
+        />
+      </div>
+    );
+  }
 }
 
 ManageCoursePage.propTypes = {
@@ -72,7 +74,7 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
-function getCourseById(courses, id){
+function getCourseById(courses, id) {
   const course = courses.filter(course => course.id == id);
   if (course.length) return course[0];
   return null;
@@ -82,7 +84,7 @@ function mapStateToProps(state, ownProps) {
 
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
 
-  if(courseId && state.courses.length > 0){
+  if (courseId && state.courses.length > 0) {
     course = getCourseById(state.courses, courseId);
   }
 
@@ -93,15 +95,15 @@ function mapStateToProps(state, ownProps) {
     };
   });
 
-    return {
-      course: course,
-      authors: authorsFormatedForDropdown
-    };
+  return {
+    course: course,
+    authors: authorsFormatedForDropdown
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-      actions: bindActionCreators(courseActions, dispatch)
+    actions: bindActionCreators(courseActions, dispatch)
   };
 }
 
